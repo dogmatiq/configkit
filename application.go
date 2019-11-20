be7f3b790441792123874bd37e9025ec5e9270f9
James Harris <mailjamesharris@gmail.com>
2019-11-21 07:52:10 +1000

    Add methods for obtaining handlers from applications.

--- application.go
+++ application.go
@@ -3,6 +3,18 @@ package configkit
 import "github.com/dogmatiq/dogma"
 
 type application interface {
+	// Handlers returns the handlers within this application.
+	Handlers() []Handler
+
+	// HandlerByIdentity by identity returns the handler with the given identity.
+	HandlerByIdentity(Identity) (Handler, bool)
+
+	// HandlerByName by name returns the handler with the given name.
+	HandlerByName(string) (Handler, bool)
+
+	// HandlerByKey by name returns the handler with the given key.
+	HandlerByKey(string) (Handler, bool)
+
 	// ForeignCommands returns the set of command message types that this
 	// application produces but does not consume.
 	ForeignCommands() []TypeName
@@ -24,6 +36,18 @@ type Application interface {
 type PortableApplication interface {
 	PortableEntity
 	application
+
+	// PortableHandlers returns the handlers within this application.
+	PortableHandlers() []PortableHandler
+
+	// PortableHandlerByIdentity by identity returns the handler with the given identity.
+	PortableHandlerByIdentity(Identity) (PortableHandler, bool)
+
+	// PortableHandlerByName by name returns the handler with the given name.
+	PortableHandlerByName(string) (PortableHandler, bool)
+
+	// PortableHandlerByKey by name returns the handler with the given key.
+	PortableHandlerByKey(string) (PortableHandler, bool)
 }
 
 // RichApplication is a specialization of the Application interface that has
@@ -34,4 +58,16 @@ type RichApplication interface {
 
 	// Application returns the application implementation.
 	Application() dogma.Application
+
+	// RichHandlers returns the handlers within this application.
+	RichHandlers() []RichHandler
+
+	// RichHandlerByIdentity by identity returns the handler with the given identity.
+	RichHandlerByIdentity(Identity) (RichHandler, bool)
+
+	// RichHandlerByName by name returns the handler with the given name.
+	RichHandlerByName(string) (RichHandler, bool)
+
+	// RichHandlerByKey by name returns the handler with the given key.
+	RichHandlerByKey(string) (RichHandler, bool)
 }
