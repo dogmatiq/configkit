0a9de622aa1a40537819659e0a9fe8a9cb06b116
James Harris <mailjamesharris@gmail.com>
2019-11-25 17:36:08 +1000

    Add `config.Error`.

--- config/doc.go
+++ config/doc.go
@@ -1,3 +1,3 @@
-// Package message provides representing the configuration and structure of
-// Dogma applications and their constituent message handlers.
+// Package config provides representing and validating the configuration and
+// structure of Dogma applications and their constituent message handlers.
 package config
--- doc.go
+++ doc.go
@@ -1,1 +1,4 @@
+// Package configkit is a set of utilities for working with Dogma application
+// configurations as well as that of their constituent message handlers and the
+// messages that they operate upon.
 package configkit
