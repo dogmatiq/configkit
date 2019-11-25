e26551e4a96829df86aa4a60bfd0cd0ba820f4dd
James Harris <mailjamesharris@gmail.com>
2019-11-25 19:23:33 +1000

    Add methods for each handler type to the visitor interfaces.

--- visitor.go
+++ visitor.go
@@ -5,17 +5,17 @@ import "context"
 // Visitor is a visitor that visits configurations.
 type Visitor interface {
 	VisitApplication(context.Context, Application) error
-	// VisitAggregate(context.Context, Aggregate) error
-	// VisitProcess(context.Context, Process) error
-	// VisitIntegration(context.Context, Integration) error
-	// VisitProjection(context.Context, Projection) error
+	VisitAggregate(context.Context, Aggregate) error
+	VisitProcess(context.Context, Process) error
+	VisitIntegration(context.Context, Integration) error
+	VisitProjection(context.Context, Projection) error
 }
 
 // RichVisitor is a visitor that visits "rich" configurations.
 type RichVisitor interface {
 	VisitRichApplication(context.Context, *RichApplication) error
-	// VisitRichAggregate(context.Context, *RichAggregate) error
-	// VisitRichProcess(context.Context, *RichProcess) error
-	// VisitRichIntegration(context.Context, *RichIntegration) error
-	// VisitRichProjection(context.Context, *RichProjection) error
+	VisitRichAggregate(context.Context, *RichAggregate) error
+	VisitRichProcess(context.Context, *RichProcess) error
+	VisitRichIntegration(context.Context, *RichIntegration) error
+	VisitRichProjection(context.Context, *RichProjection) error
 }
