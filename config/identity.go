b31718c5f9f27209eba07178e981f3da3c100a9a
James Harris <mailjamesharris@gmail.com>
2019-11-25 17:28:50 +1000

    Add `config.Identity`.

---
+++ config/identity.go
@@ -0,0 +1,91 @@
+package config
+
+import (
+	"fmt"
+	"unicode"
+)
+
+// Identity is the application-defined identity of a Dogma entity.
+type Identity struct {
+	// Name is the name component of the identity.
+	//
+	// For handlers, it is unique within an application at any given version,
+	// but may be changed over time.
+	//
+	// It is allowed, but not recommended to use the same name for an
+	// application as one of its constituent handlers.
+	Name string
+
+	// Key is the key component of the identity.
+	//
+	// It is not only unique within an application, but forever immutable. It is
+	// not permitted for an application and one of its constituent handlers to
+	// share the same key.
+	Key string
+}
+
+// NewIdentity returns a new identity.
+//
+// It returns a non-nil error if either of the name or key components is invalid.
+func NewIdentity(n, k string) (Identity, error) {
+	i := Identity{n, k}
+	return i, i.Validate()
+}
+
+// MustNewIdentity returns a new identity.
+//
+// It panics if either of the name or key components is invalid.
+func MustNewIdentity(n, k string) Identity {
+	i, err := NewIdentity(n, k)
+	if err != nil {
+		panic(err)
+	}
+
+	return i
+}
+
+// IsZero returns true if the identity is the zero-value.
+func (i Identity) IsZero() bool {
+	return i.Name == "" && i.Key == ""
+}
+
+// Validate returns an error if i is not a valid identity.
+func (i Identity) Validate() error {
+	if !isValid(i.Name) {
+		return fmt.Errorf(
+			"invalid name %#v, names must be non-empty, printable UTF-8 strings with no whitespace",
+			i.Name,
+		)
+	}
+
+	if !isValid(i.Key) {
+		return fmt.Errorf(
+			"invalid key %#v, keys must be non-empty, printable UTF-8 strings with no whitespace",
+			i.Key,
+		)
+	}
+
+	return nil
+}
+
+func (i Identity) String() string {
+	return fmt.Sprintf("%s (%s)", i.Name, i.Key)
+}
+
+// isValid returns true if n is a valid application or handler name or key.
+//
+// A valid name/key is a non-empty string consisting of Unicode printable
+// characters, except whitespace.
+func isValid(n string) bool {
+	if len(n) == 0 {
+		return false
+	}
+
+	for _, r := range n {
+		if unicode.IsSpace(r) || !unicode.IsPrint(r) {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ config/identity_test.go
@@ -0,0 +1,113 @@
+package config_test
+
+import (
+	"fmt"
+
+	. "github.com/dogmatiq/configkit/config"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("type Identity", func() {
+	Describe("func NewIdentity()", func() {
+		It("returns the identity", func() {
+			i, err := NewIdentity("<name>", "<key>")
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(i).To(Equal(Identity{"<name>", "<key>"}))
+		})
+
+		It("returns an error if the name is invalid", func() {
+			_, err := NewIdentity("", "<key>")
+			Expect(err).Should(HaveOccurred())
+		})
+
+		It("returns an error if the key is invalid", func() {
+			_, err := NewIdentity("<name>", "")
+			Expect(err).Should(HaveOccurred())
+		})
+	})
+
+	Describe("func MustNewIdentity()", func() {
+		It("returns the identity", func() {
+			i := MustNewIdentity("<name>", "<key>")
+			Expect(i).To(Equal(Identity{"<name>", "<key>"}))
+		})
+
+		It("panics if the name is invalid", func() {
+			Expect(func() {
+				MustNewIdentity("", "<key>")
+			}).To(Panic())
+		})
+
+		It("panics if the key is invalid", func() {
+			Expect(func() {
+				MustNewIdentity("<name>", "")
+			}).To(Panic())
+		})
+	})
+
+	Describe("func IsZero()", func() {
+		It("returns true if the identity is empty", func() {
+			Expect(Identity{}.IsZero()).To(BeTrue())
+		})
+
+		It("returns false if the identity is not empty", func() {
+			Expect(Identity{"<name>", "<key>"}.IsZero()).To(BeFalse())
+		})
+	})
+
+	Describe("func Validate()", func() {
+		DescribeTable(
+			"it returns nil if the name and key are valid",
+			func(v string) {
+				i := Identity{v, v}
+				Expect(i.Validate()).ShouldNot(HaveOccurred())
+			},
+			Entry("ascii", "foo-bar"),
+			Entry("unicode", "😀"),
+		)
+
+		invalidEntries := []TableEntry{
+			Entry("empty", ""),
+			Entry("non-printable ascii character (newline)", "\n"),
+			Entry("non-printable ascii character (space)", " "),
+			Entry("non-printable unicode character", "\u200B"),
+		}
+
+		DescribeTable(
+			"it returns an error if the name is invalid",
+			func(v string) {
+				i := Identity{v, "<key>"}
+				Expect(i.Validate()).Should(MatchError(
+					fmt.Sprintf(
+						"invalid name %#v, names must be non-empty, printable UTF-8 strings with no whitespace",
+						v,
+					),
+				))
+			},
+			invalidEntries...,
+		)
+
+		DescribeTable(
+			"it returns an error if the key is invalid",
+			func(v string) {
+				i := Identity{"<name>", v}
+				Expect(i.Validate()).To(MatchError(
+					fmt.Sprintf(
+						"invalid key %#v, keys must be non-empty, printable UTF-8 strings with no whitespace",
+						v,
+					),
+				))
+			},
+			invalidEntries...,
+		)
+	})
+
+	Describe("func String()", func() {
+		It("returns a string representation of the identity", func() {
+			i := Identity{"<name>", "<key>"}
+			Expect(i.String()).To(Equal("<name> (<key>)"))
+		})
+	})
+})
