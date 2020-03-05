66c8f3e06d0e5c4b0d9b77f75d821a243fcc00cf
James Harris <mailjamesharris@gmail.com>
2020-03-05 10:24:00 +1000

    Rename `Observer` to `ClientObserver`.

---
+++ api/discovery/client.go
@@ -0,0 +1,142 @@
+package discovery
+
+import (
+	"sync"
+
+	"github.com/dogmatiq/configkit/api"
+)
+
+// ClientObserver is notified when connections to config API servers are
+// established and severed.
+type ClientObserver interface {
+	// ClientConnected is called when a connection to a config API server is
+	// established.
+	ClientConnected(*api.Client)
+
+	// ClientDisconnected is called when a connection to a config API server is
+	// severed.
+	ClientDisconnected(*api.Client)
+}
+
+// ClientPublisher is an interface that allows client observers to be registered to
+// receive notifications.
+type ClientPublisher interface {
+	// RegisterClientObserver registers o to be notified when connections to
+	// config API servers are established and servered.
+	RegisterClientObserver(o ClientObserver)
+
+	// UnregisterClientObserver stops o from being notified when connections to
+	// config API servers are established and servered.
+	UnregisterClientObserver(o ClientObserver)
+}
+
+// ClientObserverSet is an client observer that publishes to other observers.
+//
+// It implements both the ClientObserver and ClientPublisher interfaces.
+type ClientObserverSet struct {
+	m         sync.RWMutex
+	observers map[ClientObserver]struct{}
+	clients   map[*api.Client]struct{}
+}
+
+// RegisterClientObserver registers o to be notified when connections to config
+// API servers are established and servered.
+func (s *ClientObserverSet) RegisterClientObserver(o ClientObserver) {
+	s.m.Lock()
+	defer s.m.Unlock()
+
+	if s.observers == nil {
+		s.observers = map[ClientObserver]struct{}{}
+	} else if _, ok := s.observers[o]; ok {
+		return
+	}
+
+	s.observers[o] = struct{}{}
+	s.notifyOne(ClientObserver.ClientConnected, o)
+}
+
+// UnregisterClientObserver stops o from being notified when connections to
+// config API servers are established and servered.
+func (s *ClientObserverSet) UnregisterClientObserver(o ClientObserver) {
+	s.m.Lock()
+	defer s.m.Unlock()
+
+	if _, ok := s.observers[o]; !ok {
+		return
+	}
+
+	delete(s.observers, o)
+	s.notifyOne(ClientObserver.ClientDisconnected, o)
+}
+
+// ClientConnected is called when a connection to a config API server is
+// established.
+func (s *ClientObserverSet) ClientConnected(c *api.Client) {
+	s.m.Lock()
+	defer s.m.Unlock()
+
+	if s.clients == nil {
+		s.clients = map[*api.Client]struct{}{}
+	} else if _, ok := s.clients[c]; ok {
+		return
+	}
+
+	s.clients[c] = struct{}{}
+	s.notifyAll(ClientObserver.ClientConnected, c)
+}
+
+// ClientDisconnected is called when a connection to a config API server is
+// servered.
+func (s *ClientObserverSet) ClientDisconnected(c *api.Client) {
+	s.m.Lock()
+	defer s.m.Unlock()
+
+	if _, ok := s.clients[c]; !ok {
+		return
+	}
+
+	delete(s.clients, c)
+	s.notifyAll(ClientObserver.ClientDisconnected, c)
+}
+
+// notifyAll notifies all observers about a change to one client.
+func (s *ClientObserverSet) notifyAll(
+	fn func(ClientObserver, *api.Client),
+	c *api.Client,
+) {
+	var g sync.WaitGroup
+
+	g.Add(len(s.observers))
+
+	for o := range s.observers {
+		o := o // capture loop variable
+
+		go func() {
+			defer g.Done()
+			fn(o, c)
+		}()
+	}
+
+	g.Wait()
+}
+
+// notifyOne notifies one observer about a change to all clients.
+func (s *ClientObserverSet) notifyOne(
+	fn func(ClientObserver, *api.Client),
+	o ClientObserver,
+) {
+	var g sync.WaitGroup
+
+	g.Add(len(s.clients))
+
+	for c := range s.clients {
+		c := c // capture loop variable
+
+		go func() {
+			defer g.Done()
+			fn(o, c)
+		}()
+	}
+
+	g.Wait()
+}
---
+++ api/discovery/client_test.go
@@ -0,0 +1,200 @@
+package discovery_test
+
+import (
+	"sync"
+
+	"github.com/dogmatiq/configkit/api"
+	. "github.com/dogmatiq/configkit/api/discovery"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var (
+	_ ClientPublisher = (*ClientObserverSet)(nil)
+	_ ClientObserver  = (*ClientObserverSet)(nil)
+)
+
+var _ = Describe("type ClientObserverSet", func() {
+	var (
+		set              *ClientObserverSet
+		obs1, obs2       *clientObserver
+		client1, client2 *api.Client
+	)
+
+	BeforeEach(func() {
+		set = &ClientObserverSet{}
+
+		obs1 = &clientObserver{}
+		obs2 = &clientObserver{}
+
+		client1 = &api.Client{}
+		client2 = &api.Client{}
+	})
+
+	Describe("func Connected()", func() {
+		It("notifies the observers about the connection", func() {
+			var observers []ClientObserver
+
+			obs1.ClientConnectedFunc = func(c *api.Client) {
+				defer GinkgoRecover()
+				Expect(c).To(BeIdenticalTo(client1))
+
+				observers = append(observers, obs1)
+			}
+
+			obs2.ClientConnectedFunc = func(c *api.Client) {
+				defer GinkgoRecover()
+				Expect(c).To(BeIdenticalTo(client1))
+
+				observers = append(observers, obs2)
+			}
+
+			set.RegisterClientObserver(obs1)
+			set.RegisterClientObserver(obs2)
+			set.ClientConnected(client1)
+
+			Expect(observers).To(ConsistOf(obs1, obs2))
+		})
+
+		It("does nothing if the client is already connected", func() {
+			set.RegisterClientObserver(obs1)
+			set.ClientConnected(client1)
+
+			obs1.ClientConnectedFunc = func(c *api.Client) {
+				defer GinkgoRecover()
+				Fail("observer unexpectedly notified of the same connection")
+			}
+
+			set.ClientConnected(client1)
+		})
+	})
+
+	Describe("func Disconnected()", func() {
+		It("notifies the observers about the disconnection", func() {
+			var observers []ClientObserver
+
+			obs1.ClientDisconnectedFunc = func(c *api.Client) {
+				defer GinkgoRecover()
+				Expect(c).To(BeIdenticalTo(client1))
+
+				observers = append(observers, obs1)
+			}
+
+			obs2.ClientDisconnectedFunc = func(c *api.Client) {
+				defer GinkgoRecover()
+				Expect(c).To(BeIdenticalTo(client1))
+
+				observers = append(observers, obs2)
+			}
+
+			set.RegisterClientObserver(obs1)
+			set.RegisterClientObserver(obs2)
+			set.ClientConnected(client1)
+			set.ClientDisconnected(client1)
+
+			Expect(observers).To(ConsistOf(obs1, obs2))
+		})
+
+		It("does nothing if the client is not in the registry", func() {
+			set.RegisterClientObserver(obs1)
+
+			obs1.ClientDisconnectedFunc = func(c *api.Client) {
+				defer GinkgoRecover()
+				Fail("observer unexpectedly notified of unknown client")
+			}
+
+			set.ClientDisconnected(client1)
+		})
+	})
+
+	Describe("func Add()", func() {
+		It("notifies the observer about existing connections", func() {
+			set.ClientConnected(client1)
+			set.ClientConnected(client2)
+
+			var clients []*api.Client
+
+			obs1.ClientConnectedFunc = func(c *api.Client) {
+				clients = append(clients, c)
+			}
+
+			set.RegisterClientObserver(obs1)
+
+			Expect(clients).To(ConsistOf(client1, client2))
+		})
+
+		It("does nothing if the observer is already registered", func() {
+			set.ClientConnected(client1)
+			set.RegisterClientObserver(obs1)
+
+			obs1.ClientConnectedFunc = func(c *api.Client) {
+				defer GinkgoRecover()
+				Fail("observer unexpectedly notified when registered twice")
+			}
+
+			set.RegisterClientObserver(obs1)
+		})
+	})
+
+	Describe("func Remove()", func() {
+		It("synthesizes a disconnection notification for existing connections", func() {
+			set.ClientConnected(client1)
+			set.ClientConnected(client2)
+			set.RegisterClientObserver(obs1)
+
+			var clients []*api.Client
+
+			obs1.ClientDisconnectedFunc = func(c *api.Client) {
+				clients = append(clients, c)
+			}
+
+			set.UnregisterClientObserver(obs1)
+
+			Expect(clients).To(ConsistOf(client1, client2))
+		})
+
+		It("prevents the observer from receiving further notifications", func() {
+			obs1.ClientConnectedFunc = func(c *api.Client) {
+				defer GinkgoRecover()
+				Fail("unregistered observer unexpectedly notified")
+			}
+
+			set.RegisterClientObserver(obs1)
+			set.UnregisterClientObserver(obs1)
+			set.ClientConnected(client1)
+		})
+
+		It("does nothing if the observer is not already registered", func() {
+			set.ClientConnected(client1)
+
+			obs1.ClientDisconnectedFunc = func(c *api.Client) {
+				defer GinkgoRecover()
+				Fail("observer unexpectedly notified when not registered")
+			}
+
+			set.UnregisterClientObserver(obs1)
+		})
+	})
+})
+
+type clientObserver struct {
+	m                      sync.Mutex
+	ClientConnectedFunc    func(*api.Client)
+	ClientDisconnectedFunc func(*api.Client)
+}
+
+func (o *clientObserver) ClientConnected(c *api.Client) {
+	if o.ClientConnectedFunc != nil {
+		o.m.Lock()
+		defer o.m.Unlock()
+		o.ClientConnectedFunc(c)
+	}
+}
+
+func (o *clientObserver) ClientDisconnected(c *api.Client) {
+	if o.ClientDisconnectedFunc != nil {
+		o.m.Lock()
+		defer o.m.Unlock()
+		o.ClientDisconnectedFunc(c)
+	}
+}
