e127f1b6845f2becff81a88bb5fc182993ccda8c
Danil Petrov <danilpetrovvi@gmail.com>
2020-11-27 19:13:33 +1000

    Add entity.Application and entity.Handler tests.
    This commit adds unit tests for the following `entity.Application` and
    `entity.Handler` methods:
    
        - Application.Identity()
        - Application.TypeName()
        - Application.MessageNames()
        - Application.Handlers()
    
        - Handler.Identity()
        - Handler.TypeName()
        - Handler.MessageNames()
        - Handler.HandlerType()

--- internal/entity/entity_test.go
+++ internal/entity/entity_test.go
@@ -1,11 +1,17 @@
-package entity
+package entity_test
 
 import (
 	"context"
 	"errors"
+	"reflect"
 
 	"github.com/dogmatiq/configkit"
 	. "github.com/dogmatiq/configkit/fixtures"
+	cfixtures "github.com/dogmatiq/configkit/fixtures"
+	. "github.com/dogmatiq/configkit/internal/entity"
+	"github.com/dogmatiq/configkit/message"
+	"github.com/dogmatiq/dogma"
+	"github.com/dogmatiq/dogma/fixtures" // can't dot-import due to conflicts
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -17,7 +23,38 @@ var _ = Describe("type Application", func() {
 		app = &Application{}
 	})
 
-	Describe("func Accept()", func() {
+	Describe("func Identity()", func() {
+		It("returns the value of .IdentityValue field", func() {
+			i := configkit.MustNewIdentity("<name>", "<key>")
+			app.IdentityValue = i
+			Expect(app.Identity()).To(Equal(i))
+		})
+	})
+
+	Describe("func TypeName()", func() {
+		It("returns the value of .TypeNameValue field", func() {
+			app.TypeNameValue = reflect.TypeOf(app).String()
+			Expect(app.TypeName()).To(Equal("*entity.Application"))
+		})
+	})
+
+	Describe("func MessageNames()", func() {
+		It("returns the value of .MessageNamesValue field", func() {
+			m := configkit.EntityMessageNames{
+				Produced: message.NameRoles{
+					cfixtures.MessageCTypeName: message.CommandRole,
+				},
+				Consumed: message.NameRoles{
+					cfixtures.MessageETypeName: message.EventRole,
+				},
+			}
+
+			app.MessageNamesValue = m
+			Expect(app.MessageNames()).To(Equal(m))
+		})
+	})
+
+	Describe("func AcceptVisitor()", func() {
 		It("calls the correct visitor method", func() {
 			v := &Visitor{
 				VisitApplicationFunc: func(_ context.Context, a configkit.Application) error {
@@ -30,6 +67,31 @@ var _ = Describe("type Application", func() {
 			Expect(err).To(Equal(err))
 		})
 	})
+
+	Describe("func Handlers()", func() {
+		It("returns the value of .HandlersValue field", func() {
+			aggregate := configkit.FromAggregate(&fixtures.AggregateMessageHandler{
+				ConfigureFunc: func(c dogma.AggregateConfigurer) {
+					c.Identity("<agg-name>", "<agg-key>")
+					c.ConsumesCommandType(fixtures.MessageC{})
+					c.ProducesEventType(fixtures.MessageE{})
+				},
+			})
+
+			projection := configkit.FromProjection(&fixtures.ProjectionMessageHandler{
+				ConfigureFunc: func(c dogma.ProjectionConfigurer) {
+					c.Identity("<proj-name>", "<proj-key>")
+					c.ConsumesEventType(fixtures.MessageE{})
+				},
+			})
+
+			s := configkit.NewHandlerSet(aggregate, projection)
+
+			app.HandlersValue = s
+			Expect(app.Handlers()).To(Equal(s))
+
+		})
+	})
 })
 
 var _ = Describe("type Handler", func() {
@@ -39,7 +101,45 @@ var _ = Describe("type Handler", func() {
 		hnd = &Handler{}
 	})
 
-	Describe("func Accept()", func() {
+	Describe("func Identity()", func() {
+		It("returns the value of .IdentityValue field", func() {
+			i := configkit.MustNewIdentity("<name>", "<key>")
+			hnd.IdentityValue = i
+			Expect(hnd.Identity()).To(Equal(i))
+		})
+	})
+
+	Describe("func TypeName()", func() {
+		It("returns the value of .TypeNameValue field", func() {
+			hnd.TypeNameValue = reflect.TypeOf(hnd).String()
+			Expect(hnd.TypeName()).To(Equal("*entity.Handler"))
+		})
+	})
+
+	Describe("func MessageNames()", func() {
+		It("returns the value of .MessageNamesValue field", func() {
+			m := configkit.EntityMessageNames{
+				Produced: message.NameRoles{
+					cfixtures.MessageCTypeName: message.CommandRole,
+				},
+				Consumed: message.NameRoles{
+					cfixtures.MessageETypeName: message.EventRole,
+				},
+			}
+
+			hnd.MessageNamesValue = m
+			Expect(hnd.MessageNames()).To(Equal(m))
+		})
+	})
+
+	Describe("func HandlerType()", func() {
+		It("returns the value of .HandlerTypeValue field", func() {
+			hnd.HandlerTypeValue = configkit.AggregateHandlerType
+			Expect(hnd.HandlerType()).To(Equal(configkit.AggregateHandlerType))
+		})
+	})
+
+	Describe("func AcceptVisitor()", func() {
 		var visitor *Visitor
 
 		BeforeEach(func() {
