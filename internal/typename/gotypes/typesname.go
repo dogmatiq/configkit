f7f371c5b7b1b0a252294f7ed9709f0b700e77ea
Danil Petrov <danilpetrovvi@gmail.com>
2020-12-21 12:31:52 +1000

    Add panicking to gotypes.NameOf() func.

--- internal/typename/gotypes/typesname.go
+++ internal/typename/gotypes/typesname.go
@@ -1,6 +1,7 @@
 package gotypes
 
 import (
+	"fmt"
 	"go/types"
 )
 
@@ -27,9 +28,13 @@ func NameOf(t types.Type) string {
 		return buildStructName(t)
 	case *types.Signature:
 		return "func" + buildFuncSignature(t)
-	default:
-		return t.String()
 	}
+	// COVERAGE NOTE: this panic is only possible if additional implementation
+	// of go/types.Type interface is introduced to represent the first-class type
+	// in Go. Not all go/types.Type interface implementations represent
+	// first-class types. For instance, go/types.Tuple can only be a part of
+	// signatures or multiple assignments.
+	panic(fmt.Sprintf("unknown type %s", t))
 }
 
 func buildChanName(c *types.Chan) string {
