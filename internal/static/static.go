4e771dbbbdb544afdc7fb935dcd5d0498c441148
Danil Petrov <danilpetrovvi@gmail.com>
2021-01-09 19:27:46 +1000

    Relocate Dogma packae import check.
    This commit relocates the Dogma package import check from `fromSSAPackages()`
    function to `FromPackages()` function so it happens only once following the
    application build.
    
    This relocation requires some slight modification of the `fromSSAPackages()`
    function's signature to accomodate the pointer to the object of `ssa.Package`
    type that carries the information about the imported `github.com/dogmatiq/dogma`
    package.

--- internal/static/static.go
+++ internal/static/static.go
@@ -20,12 +20,19 @@ func FromPackages(pkgs []*packages.Package) []configkit.Application {
 	prog, packages := ssautil.AllPackages(pkgs, ssa.SanityCheckFunctions)
 	prog.Build()
 
-	return fromSSAPackages(packages)
+	if dogmaPkg := prog.ImportedPackage(dogmaPkgPath); dogmaPkg != nil {
+		return fromSSAPackages(dogmaPkg, packages)
+	}
+
+	// If Dogma package is not found as an import, none of the packages in the
+	// built application implement dogma.Application interface that requires the
+	// import of the github.com/dogmatiq/dogma package.
+	return nil
 }
 
 // fromSSAPackages loads the list of configkit.Application from the given list
 // of ssa.Package items.
-func fromSSAPackages(pkgs []*ssa.Package) []configkit.Application {
+func fromSSAPackages(dogmaPkg *ssa.Package, pkgs []*ssa.Package) []configkit.Application {
 	var result []configkit.Application
 
 	for _, pkg := range pkgs {
@@ -35,33 +42,31 @@ func fromSSAPackages(pkgs []*ssa.Package) []configkit.Application {
 			continue
 		}
 
-		if p := pkg.Prog.ImportedPackage(dogmaPkgPath); p != nil {
-			a := p.Pkg.Scope().Lookup("Application")
-			iface := a.Type().Underlying().(*types.Interface)
+		a := dogmaPkg.Pkg.Scope().Lookup("Application")
+		iface := a.Type().Underlying().(*types.Interface)
 
-			for _, m := range pkg.Members {
-				// NOTE: the sequence of the if-blocks below is important as the
-				// value of a type implements the interface only if the
-				// interface-implementing methods have value receiver. Hence is
-				// the implementation check for the type value first.
-				//
-				// However, a pointer to the type implements the interface in
-				// both cases: when interface-implementing methods have value or
-				// pointer receiver.
-				if types.Implements(m.Type(), iface) {
-					result = append(
-						result,
-						parse(m, m.Type()),
-					)
-					continue
-				}
+		for _, m := range pkg.Members {
+			// NOTE: the sequence of the if-blocks below is important as the
+			// value of a type implements the interface only if the
+			// interface-implementing methods have value receiver. Hence is
+			// the implementation check for the type value first.
+			//
+			// However, a pointer to the type implements the interface in
+			// both cases: when interface-implementing methods have value or
+			// pointer receiver.
+			if types.Implements(m.Type(), iface) {
+				result = append(
+					result,
+					parse(m, m.Type()),
+				)
+				continue
+			}
 
-				if p := types.NewPointer(m.Type()); types.Implements(p, iface) {
-					result = append(
-						result,
-						parse(m, p),
-					)
-				}
+			if p := types.NewPointer(m.Type()); types.Implements(p, iface) {
+				result = append(
+					result,
+					parse(m, p),
+				)
 			}
 		}
 	}
