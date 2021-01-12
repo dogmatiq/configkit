e23e291d9fa936a267611705f57746869739e249
Danil Petrov <danilpetrovvi@gmail.com>
2021-01-12 19:37:38 +1000

    Convert const app keys to string literals.

--- internal/static/testdata/apps/single-app/app.go
+++ internal/static/testdata/apps/single-app/app.go
@@ -2,16 +2,11 @@ package app
 
 import "github.com/dogmatiq/dogma"
 
-const (
-	// AppKey is the application key.
-	AppKey = "8a6baab1-ee64-402e-a081-e43f4bebc243"
-)
-
 // App implements dogma.Application interface.
 type App struct{}
 
 // Configure configures the behavior of the engine as it relates to this
 // application.
 func (App) Configure(c dogma.ApplicationConfigurer) {
-	c.Identity("<app>", AppKey)
+	c.Identity("<app>", "8a6baab1-ee64-402e-a081-e43f4bebc243")
 }
