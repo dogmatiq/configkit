883911119e53f568864c0e444f4ce731b7c71d62
Danil Petrov <danilpetrovvi@gmail.com>
2021-01-12 19:55:36 +1000

    Relocate invalid syntax test outside the apps test directory.

---
+++ internal/static/testdata/invalid/invalid-syntax/invalid.go
@@ -0,0 +1,8 @@
+package syntax
+
+import "github.com/dogmatiq/dogma"
+
+// Below is the deliberate illegal Go syntax to test loading of the packages
+// with errors.
+<This is the deliberate illegal Go syntax>
+
