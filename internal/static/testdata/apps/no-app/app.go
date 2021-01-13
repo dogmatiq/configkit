0c2ee76ed13e7ea7260dd05915e618d60e033860
Danil Petrov <danilpetrovvi@gmail.com>
2021-01-13 10:54:56 +1000

    Improve and extend comments in test files, remove redundant test code.
    Co-authored-by: James Harris <mailjamesharris@gmail.com>

--- internal/static/testdata/apps/no-app/app.go
+++ internal/static/testdata/apps/no-app/app.go
@@ -2,6 +2,3 @@ package app
 
 // NonApp does not implement dogma.Application interface.
 type NonApp struct{}
-
-// Foo is dummy method of App.
-func (a *NonApp) Foo() {}
--- internal/static/testdata/ident/const-value-ident/app.go
+++ internal/static/testdata/ident/const-value-ident/app.go
@@ -12,8 +12,7 @@ const (
 // App implements dogma.Application interface.
 type App struct{}
 
-// Configure configures the behavior of the engine as it relates to this
-// application.
+// Configure sets the application identity using non-literal constant expressions.
 func (App) Configure(c dogma.ApplicationConfigurer) {
 	c.Identity(AppName, AppKey)
 }
