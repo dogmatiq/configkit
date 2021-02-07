50700a798a460b42523ae2b6d24c2ebb194bf810
Danil Petrov <danilpetrovvi@gmail.com>
2021-02-07 14:48:58 +1000

    Simplify aggregate handler tests.

--- static/aggregate_test.go
+++ static/aggregate_test.go
@@ -25,51 +25,38 @@ var _ = Describe("func FromPackages() (aggregate analysis)", func() {
 
 			apps := FromPackages(pkgs)
 
-			Expect(apps).To(
-				ConsistOf(
-					PointTo(
-						MatchAllFields(Fields{
-							"IdentityValue": Equal(
-								configkit.Identity{
-									Name: "<single-aggregate-app>",
-									Key:  "3bc3849b-abe0-4c4e-9db4-e48dc28c9a26",
-								},
-							),
-							"TypeNameValue": Equal(
-								"github.com/dogmatiq/configkit/static/testdata/aggregates/single-aggregate-app.App",
-							),
-							"MessageNamesValue": Equal(configkit.EntityMessageNames{}),
-							"HandlersValue": MatchAllKeys(Keys{
-								configkit.Identity{
-									Name: "<aggregate>",
-									Key:  "ef16c9d1-d7b6-4c99-a0e7-a59218e544fc",
-								}: PointTo(
-									MatchAllFields(Fields{
-										"IdentityValue": Equal(
-											configkit.Identity{
-												Name: "<aggregate>",
-												Key:  "ef16c9d1-d7b6-4c99-a0e7-a59218e544fc",
-											},
-										),
-										"TypeNameValue": Equal(
-											"github.com/dogmatiq/configkit/static/testdata/aggregates/single-aggregate-app.AggregateHandler",
-										),
-										"MessageNamesValue": MatchAllFields(Fields{
-											"Consumed": MatchAllKeys(Keys{
-												message.NameOf(fixtures.MessageA{}): Equal(message.CommandRole),
-												message.NameOf(fixtures.MessageB{}): Equal(message.CommandRole),
-											}),
-											"Produced": MatchAllKeys(Keys{
-												message.NameOf(fixtures.MessageC{}): Equal(message.EventRole),
-												message.NameOf(fixtures.MessageD{}): Equal(message.EventRole),
-											}),
-										}),
-										"HandlerTypeValue": Equal(configkit.AggregateHandlerType),
-									}),
+			Expect(apps).To(HaveLen(1))
+			Expect(apps[0].Handlers()).To(
+				MatchAllKeys(
+					Keys{
+						configkit.Identity{
+							Name: "<aggregate>",
+							Key:  "ef16c9d1-d7b6-4c99-a0e7-a59218e544fc",
+						}: PointTo(
+							MatchAllFields(Fields{
+								"IdentityValue": Equal(
+									configkit.Identity{
+										Name: "<aggregate>",
+										Key:  "ef16c9d1-d7b6-4c99-a0e7-a59218e544fc",
+									},
 								),
+								"TypeNameValue": Equal(
+									"github.com/dogmatiq/configkit/static/testdata/aggregates/single-aggregate-app.AggregateHandler",
+								),
+								"MessageNamesValue": MatchAllFields(Fields{
+									"Consumed": MatchAllKeys(Keys{
+										message.NameOf(fixtures.MessageA{}): Equal(message.CommandRole),
+										message.NameOf(fixtures.MessageB{}): Equal(message.CommandRole),
+									}),
+									"Produced": MatchAllKeys(Keys{
+										message.NameOf(fixtures.MessageC{}): Equal(message.EventRole),
+										message.NameOf(fixtures.MessageD{}): Equal(message.EventRole),
+									}),
+								}),
+								"HandlerTypeValue": Equal(configkit.AggregateHandlerType),
 							}),
-						}),
-					),
+						),
+					},
 				),
 			)
 		})
@@ -87,74 +74,49 @@ var _ = Describe("func FromPackages() (aggregate analysis)", func() {
 
 			apps := FromPackages(pkgs)
 
-			Expect(apps).To(
-				ConsistOf(
-					PointTo(
-						MatchAllFields(Fields{
-							"IdentityValue": Equal(
-								configkit.Identity{
-									Name: "<multiple-aggregate-app>",
-									Key:  "66abab58-e0cd-4d50-90c2-b0bd093ea2ba",
+			Expect(apps).To(HaveLen(1))
+			Expect(apps[0].Handlers()).To(
+				Equal(
+					configkit.HandlerSet{
+						configkit.Identity{
+							Name: "<first-aggregate>",
+							Key:  "e6300d8d-6530-405e-9729-e9ca21df23d3",
+						}: &entity.Handler{
+							IdentityValue: configkit.Identity{
+								Name: "<first-aggregate>",
+								Key:  "e6300d8d-6530-405e-9729-e9ca21df23d3",
+							},
+							TypeNameValue: "github.com/dogmatiq/configkit/static/testdata/aggregates/multiple-aggregate-app.FirstAggregateHandler",
+							MessageNamesValue: configkit.EntityMessageNames{
+								Consumed: message.NameRoles{
+									message.NameOf(fixtures.MessageA{}): message.CommandRole,
 								},
-							),
-							"TypeNameValue": Equal(
-								"github.com/dogmatiq/configkit/static/testdata/aggregates/multiple-aggregate-app.App",
-							),
-							"MessageNamesValue": Equal(configkit.EntityMessageNames{}),
-							"HandlersValue": MatchAllKeys(Keys{
-								configkit.Identity{
-									Name: "<first-aggregate>",
-									Key:  "e6300d8d-6530-405e-9729-e9ca21df23d3",
-								}: PointTo(
-									MatchAllFields(Fields{
-										"IdentityValue": Equal(
-											configkit.Identity{
-												Name: "<first-aggregate>",
-												Key:  "e6300d8d-6530-405e-9729-e9ca21df23d3",
-											},
-										),
-										"TypeNameValue": Equal(
-											"github.com/dogmatiq/configkit/static/testdata/aggregates/multiple-aggregate-app.FirstAggregateHandler",
-										),
-										"MessageNamesValue": MatchAllFields(Fields{
-											"Consumed": MatchAllKeys(Keys{
-												message.NameOf(fixtures.MessageA{}): Equal(message.CommandRole),
-											}),
-											"Produced": MatchAllKeys(Keys{
-												message.NameOf(fixtures.MessageB{}): Equal(message.EventRole),
-											}),
-										}),
-										"HandlerTypeValue": Equal(configkit.AggregateHandlerType),
-									}),
-								),
-								configkit.Identity{
-									Name: "<second-aggregate>",
-									Key:  "feeb96d0-c56b-4e58-9cd0-d393683c2ec7",
-								}: PointTo(
-									MatchAllFields(Fields{
-										"IdentityValue": Equal(
-											configkit.Identity{
-												Name: "<second-aggregate>",
-												Key:  "feeb96d0-c56b-4e58-9cd0-d393683c2ec7",
-											},
-										),
-										"TypeNameValue": Equal(
-											"github.com/dogmatiq/configkit/static/testdata/aggregates/multiple-aggregate-app.SecondAggregateHandler",
-										),
-										"MessageNamesValue": MatchAllFields(Fields{
-											"Consumed": MatchAllKeys(Keys{
-												message.NameOf(fixtures.MessageC{}): Equal(message.CommandRole),
-											}),
-											"Produced": MatchAllKeys(Keys{
-												message.NameOf(fixtures.MessageD{}): Equal(message.EventRole),
-											}),
-										}),
-										"HandlerTypeValue": Equal(configkit.AggregateHandlerType),
-									}),
-								),
-							}),
-						}),
-					),
+								Produced: message.NameRoles{
+									message.NameOf(fixtures.MessageB{}): message.EventRole,
+								},
+							},
+							HandlerTypeValue: configkit.AggregateHandlerType,
+						},
+						configkit.Identity{
+							Name: "<second-aggregate>",
+							Key:  "feeb96d0-c56b-4e58-9cd0-d393683c2ec7",
+						}: &entity.Handler{
+							IdentityValue: configkit.Identity{
+								Name: "<second-aggregate>",
+								Key:  "feeb96d0-c56b-4e58-9cd0-d393683c2ec7",
+							},
+							TypeNameValue: "github.com/dogmatiq/configkit/static/testdata/aggregates/multiple-aggregate-app.SecondAggregateHandler",
+							MessageNamesValue: configkit.EntityMessageNames{
+								Consumed: message.NameRoles{
+									message.NameOf(fixtures.MessageC{}): message.CommandRole,
+								},
+								Produced: message.NameRoles{
+									message.NameOf(fixtures.MessageD{}): message.EventRole,
+								},
+							},
+							HandlerTypeValue: configkit.AggregateHandlerType,
+						},
+					},
 				),
 			)
 		})
@@ -171,20 +133,9 @@ var _ = Describe("func FromPackages() (aggregate analysis)", func() {
 			Expect(err).NotTo(HaveOccurred())
 
 			apps := FromPackages(pkgs)
-			Expect(apps).To(ConsistOf(
-				&entity.Application{
-					IdentityValue: configkit.Identity{
-						Name: "<nil-aggregate-app>",
-						Key:  "250738eb-7728-4414-a850-1afefb04dbb4",
-					},
-					TypeNameValue: "github.com/dogmatiq/configkit/static/testdata/aggregates/nil-aggregate-app.App",
-					MessageNamesValue: configkit.EntityMessageNames{
-						Produced: nil,
-						Consumed: nil,
-					},
-					HandlersValue: configkit.HandlerSet{},
-				},
-			))
+
+			Expect(apps).To(HaveLen(1))
+			Expect(apps[0].Handlers()).To(Equal(configkit.HandlerSet{}))
 		})
 	})
 
@@ -200,49 +151,30 @@ var _ = Describe("func FromPackages() (aggregate analysis)", func() {
 
 			apps := FromPackages(pkgs)
 
-			Expect(apps).To(
-				ConsistOf(
-					PointTo(
-						MatchAllFields(Fields{
-							"IdentityValue": Equal(
-								configkit.Identity{
-									Name: "<nil-message-aggregate-app>",
-									Key:  "77199eb1-c893-4151-aab8-5088f6db4ea6",
+			Expect(apps).To(HaveLen(1))
+			Expect(apps[0].Handlers()).To(
+				Equal(
+					configkit.HandlerSet{
+						configkit.Identity{
+							Name: "<nil-message-aggregate>",
+							Key:  "99271492-1ec3-475f-b154-3e69cda11155",
+						}: &entity.Handler{
+							IdentityValue: configkit.Identity{
+								Name: "<nil-message-aggregate>",
+								Key:  "99271492-1ec3-475f-b154-3e69cda11155",
+							},
+							TypeNameValue: "github.com/dogmatiq/configkit/static/testdata/aggregates/nil-message-aggregate-app.AggregateHandler",
+							MessageNamesValue: configkit.EntityMessageNames{
+								Consumed: message.NameRoles{
+									message.NameOf(fixtures.MessageA{}): message.CommandRole,
 								},
-							),
-							"TypeNameValue": Equal(
-								"github.com/dogmatiq/configkit/static/testdata/aggregates/nil-message-aggregate-app.App",
-							),
-							"MessageNamesValue": Equal(configkit.EntityMessageNames{}),
-							"HandlersValue": MatchAllKeys(Keys{
-								configkit.Identity{
-									Name: "<nil-message-aggregate>",
-									Key:  "99271492-1ec3-475f-b154-3e69cda11155",
-								}: PointTo(
-									MatchAllFields(Fields{
-										"IdentityValue": Equal(
-											configkit.Identity{
-												Name: "<nil-message-aggregate>",
-												Key:  "99271492-1ec3-475f-b154-3e69cda11155",
-											},
-										),
-										"TypeNameValue": Equal(
-											"github.com/dogmatiq/configkit/static/testdata/aggregates/nil-message-aggregate-app.AggregateHandler",
-										),
-										"MessageNamesValue": MatchAllFields(Fields{
-											"Consumed": MatchAllKeys(Keys{
-												message.NameOf(fixtures.MessageA{}): Equal(message.CommandRole),
-											}),
-											"Produced": MatchAllKeys(Keys{
-												message.NameOf(fixtures.MessageB{}): Equal(message.EventRole),
-											}),
-										}),
-										"HandlerTypeValue": Equal(configkit.AggregateHandlerType),
-									}),
-								),
-							}),
-						}),
-					),
+								Produced: message.NameRoles{
+									message.NameOf(fixtures.MessageB{}): message.EventRole,
+								},
+							},
+							HandlerTypeValue: configkit.AggregateHandlerType,
+						},
+					},
 				),
 			)
 		})
